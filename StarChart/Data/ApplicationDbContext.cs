6a04c92054421eec42a5f6300173169c92000c8b
Brandon Joplin <Brandon.Joplin@wolterskluwer.com>
2022-03-09 13:33:08 -0600

    finished section 3

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StarChart.Models;
 
 namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        #region DbSets
+        public DbSet<CelestialObject> CelestialObjects { get; set; }
+        #endregion
+
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
